7dd593a7f17c5f334287b14e47228634c41e80a4
Valleyard <mynzhasardimash@gmail.com>
2025-12-13 13:03:40 +0500

    Light BG toaster

--- app/providers.tsx
+++ app/providers.tsx
@@ -44,7 +44,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
-            <Toaster position="top-right" richColors />
+            <Toaster position="top-right" richColors theme="light" />
             {process.env.NODE_ENV === 'development' && (
                 <ReactQueryDevtools initialIsOpen={false} />
             )}
